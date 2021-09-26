f0bc744126883cf288f759100191d7290e477997
M Sazzadul Hoque <7600764+sazzad16@users.noreply.github.com>
2021-09-26 17:35:57 +0600

    New Jedis Idea (commit for sharing)

--- src/test/java/redis/clients/jedis/tests/ConnectionTest.java
+++ src/test/java/redis/clients/jedis/tests/ConnectionTest.java
@@ -6,14 +6,15 @@ import static org.junit.Assert.fail;
 import org.junit.After;
 import org.junit.Test;
 
-import redis.clients.jedis.Connection;
+import redis.clients.jedis.JedisConnection;
 import redis.clients.jedis.Protocol;
 import redis.clients.jedis.Protocol.Command;
 import redis.clients.jedis.commands.ProtocolCommand;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 
 public class ConnectionTest {
-  private Connection client;
+
+  private JedisConnection client;
 
   @After
   public void tearDown() throws Exception {
@@ -23,48 +24,33 @@ public class ConnectionTest {
   }
 
   @Test(expected = JedisConnectionException.class)
-  public void checkUnkownHostBackwardCompatible() {
-    client = new Connection();
-    client.setHost("someunknownhost");
-    client.connect();
-  }
-
-  @Test(expected = JedisConnectionException.class)
   public void checkUnkownHost() {
-    client = new Connection("someunknownhost", Protocol.DEFAULT_PORT);
-    client.connect();
-  }
-
-  @Test(expected = JedisConnectionException.class)
-  public void checkWrongPortBackwardCompatible() {
-    client = new Connection();
-    client.setHost("localhost");
-    client.setPort(55665);
+    client = new JedisConnection("someunknownhost", Protocol.DEFAULT_PORT);
     client.connect();
   }
 
   @Test(expected = JedisConnectionException.class)
   public void checkWrongPort() {
-    client = new Connection(Protocol.DEFAULT_HOST, 55665);
+    client = new JedisConnection(Protocol.DEFAULT_HOST, 55665);
     client.connect();
   }
 
   @Test
   public void connectIfNotConnectedWhenSettingTimeoutInfinite() {
-    client = new Connection("localhost", 6379);
+    client = new JedisConnection("localhost", 6379);
     client.setTimeoutInfinite();
   }
 
   @Test
   public void checkCloseable() {
-    client = new Connection("localhost", 6379);
+    client = new JedisConnection("localhost", 6379);
     client.connect();
     client.close();
   }
 
   @Test
   public void getErrorMultibulkLength() throws Exception {
-    class TestConnection extends Connection {
+    class TestConnection extends JedisConnection {
       public TestConnection() {
         super("localhost", 6379);
       }
@@ -82,32 +68,6 @@ public class ConnectionTest {
       fail("Should throw exception");
     } catch (JedisConnectionException jce) {
       assertEquals("ERR Protocol error: invalid multibulk length", jce.getMessage());
-    }
-  }
-
-  @Test
-  public void readWithBrokenConnection() {
-    class BrokenConnection extends Connection {
-      private BrokenConnection() {
-        super("nonexistinghost", 0);
-        try {
-          connect();
-          fail("Client should fail connecting to nonexistinghost");
-        } catch (JedisConnectionException ignored) {
-        }
-      }
-
-      private Object read() {
-        return readProtocolWithCheckingBroken();
-      }
-    }
-
-    BrokenConnection conn = new BrokenConnection();
-    try {
-      conn.read();
-      fail("Read should fail as connection is broken");
-    } catch (JedisConnectionException jce) {
-      assertEquals("Attempting to read from a broken connection", jce.getMessage());
     }
   }
 }
